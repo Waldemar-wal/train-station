88b5ba0080127fb3bc93e09f5d5b7b65afc0ff24
Vova Savenkov <sava.v80@gmail.com>
2025-07-15 13:01:40 +0300

    Dockerizing DRF station

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,19 @@
-FROM python:3.11-slim
-LABEL maintainer="sava.v80@gmail.com"
-
-ENV PYTHONUNBUFFERED 1
+FROM python:3.13-slim
 
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
-
 RUN pip install -r requirements.txt
 
 COPY . .
+RUN mkdir -p /files/media
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+RUN adduser \
+    --disabled-password \
+    --no-create-home \
+    my_user
+
+RUN chown -R my_user /files/media
+RUN chmod -R 755 /files/media
+
+USER my_user
--- station/management/commands/wait_for_db.py
+++ station/management/commands/wait_for_db.py
@@ -1,20 +1,23 @@
 import time
-from django.db import connections
-from django.db.utils import OperationalError
+
 from django.core.management.base import BaseCommand
+from django.db import connections, OperationalError
 
 
 class Command(BaseCommand):
-    help = "Waits for database to be available before starting"
+    """Checks if connection to the default
+     database was established, retries until successful"""
 
-    def handle(self, *args, **kwargs):
-        self.stdout.write("Waiting for database...")
+    def handle(self, *args, **options):
+        self.stdout.write("Waiting for database to become available...")
+
         db_conn = None
-        while not db_conn:
+        while db_conn is None:
             try:
                 db_conn = connections["default"]
                 db_conn.cursor()
             except OperationalError:
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
-        self.stdout.write(self.style.SUCCESS("Database is available!"))
+
+        self.stdout.write(self.style.SUCCESS("Database available."))
