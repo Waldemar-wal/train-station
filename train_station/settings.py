e26e41aa70e711f7a5cc14cce14c80750121934f
Vova Savenkov <sava.v80@gmail.com>
2025-07-17 14:14:41 +0300

    update settings

--- train_station/settings.py
+++ train_station/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5)b##ej+7k&twa^_j2r03%3rkyp#v(7k2y@2z$nsb$k5aefa6r"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
